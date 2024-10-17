c6055b91d9180d4f52fedc9c054c701eb575aa9d
Victoria Kastanenka <vkastanenka@gmail.com>
2024-10-16 23:04:56 -0400

    added first server action

---
+++ src/lib/api-endpoints.ts
@@ -0,0 +1,3 @@
+// users
+export const USERS_TEST = '/users/test'
+export const USERS_GET_USER_BY_ID = '/users'
---
+++ src/lib/types.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: string
+  name: string
+  email: string
+  username: string
+  image: string
+  headline: string
+  bio: string
+}
