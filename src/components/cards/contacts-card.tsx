02a8219a0ea8f5035511f9d3f987336720e00bf1
Victoria Kastanenka <vkastanenka@gmail.com>
2024-10-22 23:16:25 -0400

    updated contacts card

--- src/components/cards/contacts-card.tsx
+++ src/components/cards/contacts-card.tsx
@@ -1,3 +1,12 @@
-export const ContactsCard: React.FC = () => {
-  return <div>Contacts card</div>
+// components
+import { Card } from '@/components/primitives/card'
+import { Typography } from '@/components/ui/typography'
+
+export const ContactsCard = () => {
+  return (
+    <Card>
+      <Typography.H3>Meet new contacts</Typography.H3>
+      <p>Click here to get started</p>
+    </Card>
+  )
 }
