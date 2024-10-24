8441def2db9323c93ffcbfef9acb41411b2245cd
Victoria Kastanenka <vkastanenka@gmail.com>
2024-10-24 19:53:14 -0400

    added github repositories error handling

--- src/components/cards/user-github-repositories-card.tsx
+++ src/components/cards/user-github-repositories-card.tsx
@@ -13,6 +13,9 @@ import GithubRainbow from '/public/svg/github-rainbow.svg'
 // utils
 import { useState, useEffect } from 'react'
 import { getUserGithubRepositories } from '@/src/lib/actions/user'
+import { useToast } from '@/hooks/use-toast'
+
+// types
 import {
   GetUserGithubRepositoriesResponse,
   GetUserGithubRepositoryResponse,
@@ -25,18 +28,39 @@ interface UserCard {
 export const UserGithubRepositoriesCard: React.FC<UserCard> = ({
   githubRepositories,
 }) => {
+  const { toast } = useToast()
+
   const [repositories, setRepositories] =
     useState<GetUserGithubRepositoriesResponse>()
 
   useEffect(() => {
     const getRepos = async () => {
       const repositories = await getUserGithubRepositories(githubRepositories)
+
+      const failedRequestUrls: string[] = []
+
+      repositories.forEach((repo) => {
+        if (!(repo as GetUserGithubRepositoryResponse).status) {
+          failedRequestUrls.push(repo.url)
+        }
+      })
+
+      if (failedRequestUrls?.length) {
+        toast({
+          title: 'Error!',
+          description: (
+            <ErrorDescription failedRequestUrls={failedRequestUrls} />
+          ),
+          variant: 'destructive',
+        })
+      }
+
       setRepositories(repositories)
     }
     if (githubRepositories.length) {
       getRepos()
     }
-  }, [githubRepositories])
+  }, [toast, githubRepositories])
 
   return (
     <Card className="flex flex-col gap-4">
@@ -44,31 +68,34 @@ export const UserGithubRepositoriesCard: React.FC<UserCard> = ({
         <Typography.H4>Github Repositories</Typography.H4>
         <GithubRainbow />
       </div>
+      {/* No repositories in their profile */}
       {!githubRepositories.length && (
         <Typography.P>
           This user has not added any Github repositories to their profile.
         </Typography.P>
       )}
+      {/* Repositories in profile but no response yet */}
       {githubRepositories.length && !repositories?.length && (
         <div className="flex gap-4">
-          <CardShadCn className="basis-1/2 p-[18px]">
+          <CardShadCn className="sm:basis-1/2 p-[18px]">
             <Skeleton className="h-[40px] w-full" />
           </CardShadCn>
-          <CardShadCn className="basis-1/2 p-[18px]">
+          <CardShadCn className="sm:basis-1/2 p-[18px]">
             <Skeleton className="h-[40px] w-full" />
           </CardShadCn>
         </div>
       )}
-      <div className="flex gap-4">
-        {repositories &&
-          repositories.map((repository, i) => {
-            // if (!(repository as { url: string; success: boolean }).success) {
-            //   return null // TODO: Proper error handling
-            // }
+      {/* Repositories in profile and response */}
+      {repositories && (
+        <div className="flex flex-col sm:flex-row gap-4">
+          {repositories.map((repository, i) => {
+            if (!(repository as GetUserGithubRepositoryResponse).status) {
+              return null
+            }
             const repositorySuccess =
               repository as GetUserGithubRepositoryResponse
             return (
-              <CardShadCn key={i} className="basis-1/2">
+              <CardShadCn key={i} className="sm:basis-1/2">
                 <Link
                   className="p-[18px] h-full w-full"
                   href={repositorySuccess.data.html_url}
@@ -86,7 +113,22 @@ export const UserGithubRepositoriesCard: React.FC<UserCard> = ({
               </CardShadCn>
             )
           })}
-      </div>
+        </div>
+      )}
     </Card>
   )
 }
+
+const ErrorDescription: React.FC<{ failedRequestUrls?: string[] }> = ({
+  failedRequestUrls,
+}) => (
+  <Typography.Muted>
+    <span className="block">Failed to fetch repositories from:</span>
+    {failedRequestUrls?.map((url, i) => (
+      <span key={i} className="block">
+        {url}
+      </span>
+    ))}
+    <span className="block">Please check the urls or try later.</span>
+  </Typography.Muted>
+)
