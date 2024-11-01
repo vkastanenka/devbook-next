4d47e047714d43cec4ecadfcf32f5bfbf6c4364c
Victoria Kastanenka <vkastanenka@gmail.com>
2024-11-01 13:52:45 -0400

    added post types

--- src/types/post-types.ts
+++ src/types/post-types.ts
@@ -1,3 +1,30 @@
+import { User } from '@/types/user-types'
+
+export interface Comment {
+  id: string
+  body: string
+  createdAt: Date
+  updatedAt: Date
+  parentComment?: Comment
+  parentCommentId?: string
+  post?: Post
+  postId: string
+  user?: User
+  userId: string
+  commentLikes?: CommentLike[]
+  subComments?: Comment[]
+}
+
+export interface CommentLike {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+  comment?: Comment
+  commentId: string
+  user?: User
+  userId: string
+}
+
 export enum PostType {
   DEFAULT,
   EVENT,
@@ -8,11 +35,20 @@ export interface Post {
   id: string
   body: string
   postType: PostType
-  // user
-  // userId
   createdAt: Date
   updatedAt: Date
-  // attachments
-  // postComments
-  // postLikes
+  user?: User
+  userId: string
+  comments?: Comment[]
+  postLikes?: PostLike[]
+}
+
+export interface PostLike {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+  post?: Post
+  postId: string
+  user?: User
+  userId: string
 }
