ffa183041ed0ccb2c4670b42df3950a76b5c3b33
Victoria Kastanenka <vkastanenka@gmail.com>
2024-11-04 19:55:07 -0500

    updated endpoints

--- src/constants/api-endpoint-constants.ts
+++ src/constants/api-endpoint-constants.ts
@@ -1,12 +1,29 @@
+// addresses
+const ADDRESSES = '/addresses'
+export const ADDRESSES_CURRENT_USER_ADDRESS = `${ADDRESSES}/current-user/address`
+
 // auth
-export const AUTH_REGISTER = '/auth/register'
-export const AUTH_LOGIN = '/auth/login'
-export const AUTH_SEND_RESET_PASSWORD_TOKEN = '/auth/send-reset-password-token'
-export const AUTH_RESET_PASSWORD = '/auth/reset-password'
-export const AUTH_SESSION = '/auth/session'
+const AUTH = '/auth'
+export const AUTH_LOGIN = `${AUTH}/login`
+export const AUTH_REGISTER = `${AUTH}/register`
+export const AUTH_SEND_RESET_PASSWORD_TOKEN = `${AUTH}/send-reset-password-token`
+export const AUTH_RESET_PASSWORD = `${AUTH}/reset-password`
+export const AUTH_CURRENT_USER_SESSION = `${AUTH}/current-user/session`
+
+// posts
+const POSTS = '/posts'
+export const POSTS_CURRENT_USER_COMMENT = `${POSTS}/current-user/comment`
+export const POSTS_CURRENT_USER_COMMENT_LIKE = `${POSTS}/current-user/comment-like`
+export const POSTS_CURRENT_USER_POST = `${POSTS}/current-user/post`
+export const POSTS_CURRENT_USER_POST_LIKE = `${POSTS}/current-user/post-like`
+
+// search
+const SEARCH = '/search'
+export const SEARCH_DEVBOOK = `${SEARCH}/devbook`
 
 // users
-export const USERS_GET_CURRENT_USER = '/users/current-user'
-export const USERS_GET_DEVBOOK_SEARCH = '/users/devbook-search'
-export const USERS_GET_USERNAME = '/users/username'
-export const USERS_USER = '/users/user'
+const USERS = '/users'
+export const USERS_USERNAME = `${USERS}/username`
+export const USERS_CURRENT_USER = `${USERS}/current-user/user`
+export const USERS_CURRENT_USER_EDUCATION = `${USERS}/current-user/education`
+export const USERS_CURRENT_USER_EXPERIENCE = `${USERS}/current-user/experience`
