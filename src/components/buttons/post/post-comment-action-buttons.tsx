8b9d0686422a51060c359223fd2404ba4dac55e4
Victoria Kastanenka <vkastanenka@gmail.com>
2024-11-18 02:18:45 -0500

    removed comment likes

--- src/components/buttons/post/post-comment-action-buttons.tsx
+++ src/components/buttons/post/post-comment-action-buttons.tsx
@@ -1,18 +1,10 @@
 'use client'
 
-// actions
-import {
-  postCreateCurrentUserCommentLike,
-  postDeleteCurrentUserCommentLike,
-} from '@/src/actions/post-actions'
-
 // svg
-import { MessageSquareText, ThumbsUp } from 'lucide-react'
+import { MessageSquareText } from 'lucide-react'
 
 // utils
-import { useRouter } from 'next/navigation'
 import { useModal } from '@/src/hooks/use-modal-store'
-import { useToast } from '@/src/hooks/use-toast'
 
 // types
 import { Comment, Post } from '@/src/types/post-types'
@@ -29,55 +21,10 @@ export const CommentActionButtons: React.FC<CommentActionButtons> = ({
   currentUser,
   post,
 }) => {
-  const router = useRouter()
-  const { toast } = useToast()
   const { onOpen } = useModal()
-
-  const likeComment = async () => {
-    if (currentUser) {
-      let commentLikeId, response
-
-      comment.commentLikes?.every((commentLike) => {
-        if (commentLike.userId === currentUser.id) {
-          commentLikeId = commentLike.id
-          return false
-        }
-        return true
-      })
-
-      if (commentLikeId) {
-        response = await postDeleteCurrentUserCommentLike(commentLikeId)
-      } else {
-        response = await postCreateCurrentUserCommentLike({
-          commentId: comment.id,
-          userId: currentUser.id,
-        })
-      }
-
-      if (!response.success) {
-        toast({
-          title: 'Error!',
-          description: response.message,
-          variant: 'destructive',
-        })
-        return
-      }
-
-      router.refresh()
-
-      toast({
-        title: 'Success!',
-        description: response.message,
-      })
-    }
-  }
 
   return (
     <div className="flex items-center gap-2">
-      <button className="muted flex gap-1 items-center" onClick={likeComment}>
-        <ThumbsUp className="w-4" />
-        Like
-      </button>
       <button
         className="muted flex gap-1 items-center"
         onClick={() =>
